a23ebc6de0281eda7f3533b20266fd11dbd797b0
atabiksf <atabik.shofi@gmail.com>
2022-03-20 16:09:57 +0700

    fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -51,17 +51,17 @@ int main() {
         cin >> command;
 
         if (command == "EXPORT") {
-        string outputPath;
-        cin >> outputPath;
-        ofstream outputFile(outputPath);
+            string outputPath;
+            cin >> outputPath;
+            ofstream outputFile(outputPath);
 
-        // hardcode for first test case
-        outputFile << "21:10" << endl;
-        outputFile << "6:1" << endl;
-        for (int i = 2; i < 27; i++) {
-            outputFile << "0:0" << endl;
-        }
-        cout << "Exported" << endl;
+            // hardcode for first test case
+            outputFile << "21:10" << endl;
+            outputFile << "6:1" << endl;
+            for (int i = 2; i < 27; i++) {
+                outputFile << "0:0" << endl;
+            }
+            cout << "Exported" << endl;
         } 
         else if (command == "CRAFT") {
             cout << "TODO" << endl;
