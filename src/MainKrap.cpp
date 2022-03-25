9957efaea256ece1797c3514818229a2de25bce4
atabiksf <atabik.shofi@gmail.com>
2022-03-25 13:28:32 +0700

    update MainKrap

--- src/MainKrap.cpp
+++ src/MainKrap.cpp
@@ -17,7 +17,7 @@ MainKrap* MainKrap::getInstance(){
 void MainKrap::initialize(string configPath, string itemFile, string recipeFolder){
    this->setupConfig(configPath, itemFile);
    this->setupRecipe(configPath, recipeFolder);
-   
+
    this->command = "Default";
    this->inventory = new Inventory();
    this->craftbox = new Crafting();
@@ -102,3 +102,189 @@ void MainKrap::setupRecipe(string configPath, string recipeFolder){
    }
 }
 
+void MainKrap::GIVE(){
+   string itemName;
+   int itemQty;
+   cin >> itemName >> itemQty;
+   if (find(listTool.begin(), listTool.end(), itemName) != listTool.end()) {
+         cout << "Tidak dapat melakukan command GIVE untuk item Tool!" << endl;
+   }
+   else if (find(listNonTool.begin(), listNonTool.end(), itemName) != listNonTool.end()) {
+         ItemNonTool* item = new ItemNonTool(itemName, itemType.at(itemName), itemQty);
+         (Item*)item;
+         (*this->inventory).insertItem(item);
+   }
+   else {
+         cout << "Nama item tidak valid!!" << endl;
+   }
+}
+void MainKrap::MOVE(){
+   pair<int,int> idx_src;
+   vector<pair<int,int>> idx_dest;
+   int row_src, col_src, slotQty, row_dest, col_dest;
+   int row = (*this->inventory).getRowSize();
+   int col = (*this->inventory).getColSize();
+
+   string out = checkInput('I',row,col,row_src,col_src);
+   if (out.empty()){
+      idx_src = make_pair(row_src,col_src);
+      cin >> slotQty;
+      row = (*this->craftbox).getRowSize();
+      col = (*this->craftbox).getColSize();
+      for (int iterator = 0; iterator < slotQty; iterator++) {
+         string out = checkInput('C',row,col,row_dest,col_dest);
+         if (out.empty()){
+            idx_dest.push_back(make_pair(row_dest,col_dest));
+         } else {
+            cout << out << endl;
+            break;
+         }
+      }
+      if (idx_dest.size() == slotQty){
+         this->inventory->moveTo((*this->craftbox), idx_src, idx_dest);
+      }
+   } else {
+      row = (*this->craftbox).getRowSize();
+      col = (*this->craftbox).getColSize();
+      string out = checkInput('C',row,col,row_src,col_src);
+      if (out.empty()){
+         idx_src = make_pair(row_src,col_src);
+         cin >> slotQty;
+         if (slotQty == 1){
+            row = (*this->inventory).getRowSize();
+            col = (*this->inventory).getColSize();
+
+            string out = checkInput('I',row,col,row_dest,col_dest);
+            if (out.empty()){
+               idx_dest.push_back(make_pair(row_dest,col_dest));
+               this->craftbox->moveTo((*this->inventory),idx_src,idx_dest);
+            } else {
+               cout << out << endl;
+            }
+         }
+         else {
+            cout << "Hanya dapat memindahkan ke 1 slot inventory" << endl;
+         }
+      } else {
+         cout << out << endl;
+      }
+   }
+}
+void MainKrap::CRAFT(){
+
+}
+void MainKrap::USE(){
+   int row_dest, col_dest;
+   int row = (*this->inventory).getRowSize();
+   int col = (*this->inventory).getColSize();
+
+   string out = checkInput('I',row,col,row_dest,col_dest);
+   if (out.empty()){
+      (*this->inventory).useItem(row_dest,col_dest);
+   } else {
+      cout << out << endl;
+   }
+}
+void MainKrap::DISCARD(){
+   int row_dest, col_dest, itemQty;
+   int row = (*this->inventory).getRowSize();
+   int col = (*this->inventory).getColSize();
+
+   string out = checkInput('I',row,col,row_dest,col_dest);
+   if (out.empty()){
+      cin >> itemQty;
+      (*this->inventory).discardItem(row_dest,col_dest,itemQty);
+   } else {
+      cout << out << endl;
+   }
+}
+void MainKrap::SHOW(){
+   (*this->craftbox).displayBoxes();
+   (*this->inventory).displayBoxes();
+}
+void MainKrap::EXPORT(){
+   string outputPath;
+   cout << "Masukkan nama file baru : " << endl;
+   cin >> outputPath;
+   ifstream checkfile;
+   checkfile.open(outputPath);
+   while (checkfile){
+      checkfile.close();
+      string confirmation;
+      cout << "Terdapat file dengan nama yang sama!" << endl;
+      cout << "Apakah ingin melakukan overwrite? (Yy)" << endl;
+      cout << "(Ketik apapun untuk input nama file baru!)" << endl;
+      cin >> confirmation;
+      if (confirmation == "Y" || confirmation == "y"){
+         break;
+      }
+      else {
+         cout << "Masukkan nama file baru : " << endl;
+         cin >> outputPath;
+         checkfile.open(outputPath);
+      }
+   }
+   ofstream outputFile;
+   outputFile.open(outputPath);
+   int row = (*this->inventory).getRowSize();
+   int col = (*this->inventory).getColSize();
+
+   // Do export
+   for (int i = 0; i < row; i++){
+         for (int j = 0; j < col; j++){
+            Item* elmt = (*this->inventory).getElmt(i,j);
+            if (elmt->getTool()){
+               ItemTool* elmt = static_cast<ItemTool*>(elmt);
+               outputFile << itemId.at(elmt->getName()) << ":" << elmt->getDurability() << endl;
+            }
+            else{
+               outputFile << itemId.at(elmt->getName()) << ":" << elmt->getQuantity() << endl;
+            }
+         }
+   }
+   cout << "Exported" << endl;
+}
+void MainKrap::HELP(){
+   cout << "GIVE NAME COUNT -> Mendapatkan barang NAME sebanyak COUNT" << endl;
+   cout << "DISCARD IX COUNT -> Membuang item  sejumlah COUNT pada inventory slot X jika jumlah mencukupi" << endl;
+   cout << "MOVE IX N CA CB ... CN -> Memindahkan item dari inventory slot X ke N slot crafting" << endl;
+   cout << "MOVE IX 1 IY -> Memindahkan dan menumpuk item dari inventory slot X ke slot Y jika item sama" << endl;
+   cout << "MOVE CX 1 IY -> Memindahkan item dari crafting slot X ke inventory slot Y" << endl;
+   cout << "CRAFT -> Melakukan crafting item jika konfigurasi pada box crafting memenuhi resep" << endl;
+   cout << "USE IX -> Menggunakan item pada Inventory slot X jika item adalah item tool" << endl;
+   cout << "SHOW -> Menampilkan isi box Crafting dan Inventory" << endl;
+   cout << "EXPORT FILENAME -> Melakukan eksport data inventory ke dalam file FILENAME" << endl;
+   cout << "EXIT -> Keluar dari permainan" << endl;
+}
+
+void MainKrap::COMMAND(){
+
+}
+
+string MainKrap::checkInput(char toMatch, int rowMatch, int colMatch, int& row, int& col){
+    string dest, out;
+    cin >> dest;
+    out = "";
+    col = -1;
+    row = 0;
+    if (dest.length() > 3 || dest[0] != toMatch){
+        out = "Masukan slot inventory tidak sesuai!\n";
+    }
+    else {
+        try{
+            col = stoi(dest.substr(1));
+        } catch(exception){
+            out = "Masukan slot inventory tidak sesuai!\n";
+        }
+        while (col >= colMatch){
+            col -= colMatch;
+            row++;
+        }
+        if (row >= rowMatch){
+            out = "Indeks masukan di luar index inventory!!\n";
+        } else {
+            //do nothing (input valid then out = "")
+        }
+    }
+    return out;
+}
--- src/MainKrap.hpp
+++ src/MainKrap.hpp
@@ -1,7 +1,7 @@
 #include "./Item/Item.hpp"
 #include "./Exception/Exception.hpp"
 #include "./Boxes/Boxes.hpp"
-#include <filesystem>
+//#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -11,7 +11,7 @@
 #include <cstdlib>
 #include <bits/stdc++.h>
 using namespace std;
-using namespace filesystem;
+//using namespace filesystem;
 
 
 class MainKrap{
@@ -72,4 +72,5 @@ public:
     // Main program cek command
     void COMMAND();
 
+    string checkInput(char toMatch, int rowMatch, int colMatch, int& row, int& col);
 };
